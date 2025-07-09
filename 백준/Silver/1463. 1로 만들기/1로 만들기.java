5e8b60e76a9ee45aa5f000c6fbaf5530664ec45d
SeojunKim-pumisj <83750080+SeojunKim-pumisj@users.noreply.github.com>
2025-07-10 00:54:31 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18156 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, -1);
+        arr[1] = 0;
+
+        for(int i = 2 ; i < n + 1; i++){
+            if(i % 2 == 0){
+                arr[i] = arr[i/2] + 1;
+            }
+            if(i % 3 == 0){
+                if(arr[i] != -1){
+                    arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+                }
+                else{
+                    arr[i] = arr[i/3] + 1;
+                }
+            }
+            if(arr[i] != -1){
+                arr[i] = Math.min(arr[i], arr[i-1] + 1);
+            }
+            else{
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        System.out.println(arr[n]);
+    }
+}
