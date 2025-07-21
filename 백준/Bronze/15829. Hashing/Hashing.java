53557b272a0347a727d7485842899fd0a20926b7
SeojunKim-pumisj <83750080+SeojunKim-pumisj@users.noreply.github.com>
2025-07-22 00:21:12 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14216 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,11 +7,13 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String n = br.readLine();
         String str = br.readLine();
-        int sum = 0;
+        long sum = 0;
+        long r = 1;
         for(int i = 0 ; i < str.length(); i++){
-            sum += (int)(((int)str.charAt(i) - 96) * Math.pow(31, i));
+            sum += (long)(((long)str.charAt(i) - 96) * r);
+            r = (r * 31) % 1234567891;
         }
 
-        System.out.println(sum);
+        System.out.println(sum % 1234567891);
     }
 }
